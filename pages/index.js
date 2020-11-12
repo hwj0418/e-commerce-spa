931fa1b960cbff45bae9978d74a4b588cba47ea5
Wj <ddh0418@gmail.com>
2020-11-12 18:43:14 -0500

    adding some resoucrs

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Moon Store
+        </title>
         <link rel="icon" href="/fullmoonicon.png" />
       </Head>
 
